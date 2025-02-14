6a0af5c6c5b6401b9d3c13e257be5fe6a432cefe
hmartiins <hmartins224@gmail.com>
2025-02-14 18:15:19 -0300

    fix(login): add SingleChildScrollView

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -11,70 +11,72 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Header(
-            title: 'Faça Login com sua conta',
-            child: Text(
-              'Faça login e acesse seu perfil',
-              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                    color: Colors.white70,
-                  ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 24,
-              vertical: 32,
-            ),
-            child: Form(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'E-mail',
-                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                          color: Colors.grey.shade600,
-                        ),
-                  ),
-                  Gap(12),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Seu e-mail',
-                      prefixIcon: Icon(Icons.email_outlined),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Header(
+              title: 'Faça Login com sua conta',
+              child: Text(
+                'Faça login e acesse seu perfil',
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Colors.white70,
                     ),
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  Gap(16),
-                  Text(
-                    'Senha',
-                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                          color: Colors.grey.shade600,
-                        ),
-                  ),
-                  Gap(12),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Sua senha',
-                      prefixIcon: Icon(Icons.lock_outlined),
-                    ),
-                    obscureText: true,
-                  ),
-                  Text('Esqueci minha senha'),
-                  ElevatedButton(onPressed: () {}, child: Text('Entrar')),
-                  TextButton(
-                    onPressed: () {},
-                    child: Text('Continuar como visitante'),
-                  ),
-                ],
               ),
             ),
-          ),
-          TextButton(
-            onPressed: presenter.navigateToSignUp,
-            child: Text('Não tem uma conta? Registre-se'),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 24,
+                vertical: 32,
+              ),
+              child: Form(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'E-mail',
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color: Colors.grey.shade600,
+                          ),
+                    ),
+                    Gap(12),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Seu e-mail',
+                        prefixIcon: Icon(Icons.email_outlined),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    Gap(16),
+                    Text(
+                      'Senha',
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color: Colors.grey.shade600,
+                          ),
+                    ),
+                    Gap(12),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Sua senha',
+                        prefixIcon: Icon(Icons.lock_outlined),
+                      ),
+                      obscureText: true,
+                    ),
+                    Text('Esqueci minha senha'),
+                    ElevatedButton(onPressed: () {}, child: Text('Entrar')),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text('Continuar como visitante'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: presenter.navigateToSignUp,
+              child: Text('Não tem uma conta? Registre-se'),
+            ),
+          ],
+        ),
       ),
     );
   }
