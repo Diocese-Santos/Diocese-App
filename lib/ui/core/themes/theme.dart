f474593ced84c6486e9430d72130897221bae70c
hmartiins <hmartins224@gmail.com>
2025-02-14 14:39:46 -0300

    feat(login): create inputDecorationTheme

--- lib/ui/core/themes/theme.dart
+++ lib/ui/core/themes/theme.dart
@@ -3,6 +3,23 @@
 import 'package:flutter/material.dart';
 
 abstract final class AppTheme {
+  static InputDecorationTheme inputDecorationTheme = InputDecorationTheme(
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(12),
+      borderSide: BorderSide(color: Colors.grey.shade400),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(12),
+      borderSide: BorderSide(color: Colors.blue),
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(12),
+      borderSide: BorderSide(color: Colors.grey.shade400),
+    ),
+    prefixIconColor: Colors.grey.shade400,
+    labelStyle: TextStyle(color: Colors.grey.shade400),
+  );
+
   static ThemeData lightTheme = ThemeData(
     useMaterial3: true,
     brightness: Brightness.light,
@@ -11,6 +28,7 @@ abstract final class AppTheme {
       elevation: 0,
       centerTitle: true,
     ),
+    inputDecorationTheme: inputDecorationTheme,
     fontFamily: 'Montserrat',
     extensions: const [],
   );
--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:diocese_santos/presentation/presenters/login_presenter.dart';
 import 'package:diocese_santos/ui/widgets/header.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key, required this.presenter});
@@ -21,18 +22,52 @@ class LoginPage extends StatelessWidget {
                   ),
             ),
           ),
-          Form(
-            child: Column(
-              children: [
-                TextFormField(decoration: InputDecoration(labelText: 'E-mail')),
-                TextFormField(decoration: InputDecoration(labelText: 'Senha')),
-                Text('Esqueci minha senha'),
-                ElevatedButton(onPressed: () {}, child: Text('Entrar')),
-                TextButton(
-                  onPressed: () {},
-                  child: Text('Continuar como visitante'),
-                ),
-              ],
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 24,
+              vertical: 32,
+            ),
+            child: Form(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'E-mail',
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: Colors.grey.shade600,
+                        ),
+                  ),
+                  Gap(12),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Seu e-mail',
+                      prefixIcon: Icon(Icons.email_outlined),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  Gap(16),
+                  Text(
+                    'Senha',
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: Colors.grey.shade600,
+                        ),
+                  ),
+                  Gap(12),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Sua senha',
+                      prefixIcon: Icon(Icons.lock_outlined),
+                    ),
+                    obscureText: true,
+                  ),
+                  Text('Esqueci minha senha'),
+                  ElevatedButton(onPressed: () {}, child: Text('Entrar')),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text('Continuar como visitante'),
+                  ),
+                ],
+              ),
             ),
           ),
           TextButton(
