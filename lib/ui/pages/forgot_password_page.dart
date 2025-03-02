c8fe05d8d7c70dfbc7b663d3c97b16c9c66562ff
hmartiins <hmartins224@gmail.com>
2025-03-02 11:02:45 -0300

    feat(forgot-password): enhance layout of forgot password page with improved scrolling and structure

--- lib/ui/pages/forgot_password_page.dart
+++ lib/ui/pages/forgot_password_page.dart
@@ -15,84 +15,105 @@ class ForgotPasswordPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Form(
-        child: Column(
-          children: [
-            Header(
-              title: 'Recuperar senha',
-              child: Text(
-                'Insira seu email para resetar sua senha',
-                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                      color: Colors.white70,
-                    ),
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 24,
-                  vertical: 32,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'E-mail',
-                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                            color: Colors.grey.shade600,
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constraints.maxHeight),
+              child: IntrinsicHeight(
+                child: Form(
+                  child: Column(
+                    children: [
+                      Header(
+                        title: 'Recuperar senha',
+                        child: Text(
+                          'Insira seu email para resetar sua senha',
+                          style:
+                              Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                    color: Colors.white70,
+                                  ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 24,
+                            vertical: 32,
                           ),
-                    ),
-                    const Gap(12),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: 'Seu e-mail',
-                        prefixIcon: Icon(Icons.email_outlined),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'E-mail',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium!
+                                    .copyWith(
+                                      color: Colors.grey.shade600,
+                                    ),
+                              ),
+                              const Gap(12),
+                              TextFormField(
+                                decoration: const InputDecoration(
+                                  labelText: 'Seu e-mail',
+                                  prefixIcon: Icon(Icons.email_outlined),
+                                ),
+                                keyboardType: TextInputType.emailAddress,
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
-                      keyboardType: TextInputType.emailAddress,
-                    ),
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.all(24.0),
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 48,
+                              width: double.infinity,
+                              child: FilledButton(
+                                onPressed: presenter.handleRecoveryPassword,
+                                child: Text(
+                                  'Recuperar',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodyLarge!
+                                      .copyWith(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                ),
+                              ),
+                            ),
+                            const Gap(14),
+                            SizedBox(
+                              height: 48,
+                              width: double.infinity,
+                              child: OutlinedButton(
+                                onPressed: context.pop,
+                                child: Text(
+                                  'Cancelar',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodyLarge!
+                                      .copyWith(
+                                        color: Colors.blue,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                ),
+                              ),
+                            ),
+                            const Gap(14),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(24.0),
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 48,
-                    width: double.infinity,
-                    child: FilledButton(
-                      onPressed: presenter.handleRecoveryPassword,
-                      child: Text(
-                        'Recuperar',
-                        style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w500,
-                            ),
-                      ),
-                    ),
-                  ),
-                  const Gap(14),
-                  SizedBox(
-                    height: 48,
-                    width: double.infinity,
-                    child: OutlinedButton(
-                      onPressed: context.pop,
-                      child: Text(
-                        'Cancelar',
-                        style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                              color: Colors.blue,
-                              fontWeight: FontWeight.w500,
-                            ),
-                      ),
-                    ),
-                  ),
-                  const Gap(14),
-                ],
-              ),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
