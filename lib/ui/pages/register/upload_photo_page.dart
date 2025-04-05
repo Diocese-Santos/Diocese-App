78b3f486b946ae1bae4fb9e42209738ea70bc033
hmartiins <hmartins224@gmail.com>
2025-04-05 17:38:36 -0300

    feat(upload): implement RegisterUploadPhotoPage and presenter for photo upload functionality

---
+++ lib/ui/pages/register/upload_photo_page.dart
@@ -0,0 +1,86 @@
+import 'package:diocese_santos/main/routing/routes.dart';
+import 'package:diocese_santos/presentation/presenters/upload_photo_presenter.dart';
+import 'package:diocese_santos/ui/widgets/header.dart';
+import 'package:diocese_santos/ui/widgets/page_container.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
+
+class RegisterUploadPhotoPage extends StatelessWidget {
+  const RegisterUploadPhotoPage({required this.presenter, super.key});
+
+  final RegisterUploadPhotoPresenter presenter;
+
+  @override
+  Widget build(BuildContext context) {
+    return PageContainer(
+      isAFormPage: true,
+      children: [
+        Header(
+          title: 'Foto',
+          child: Text(
+            'Insira uma foto sua para ser mostrado no seu perfil',
+            style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  color: Colors.white70,
+                ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 24,
+              vertical: 32,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [],
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 24,
+            vertical: 32,
+          ),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 48,
+                width: double.infinity,
+                child: FilledButton(
+                  onPressed: presenter.uploadPhoto,
+                  child: Text(
+                    'Criar Conta',
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                        ),
+                  ),
+                ),
+              ),
+              Gap(38),
+              TextButton(
+                onPressed: () => context.go(Routes.home),
+                child: Text.rich(
+                  TextSpan(
+                    text: "Já tem uma conta? ",
+                    style: TextStyle(color: Colors.grey.shade500),
+                    children: [
+                      TextSpan(
+                        text: "Faça login",
+                        style: const TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
