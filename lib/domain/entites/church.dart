bf668936f1dec26e350936b7edc581ab747d3fcf
Henrique Martins <hmartins.dev@gmail.com>
2025-06-02 00:16:25 -0300

    feat: add Church, Event, and LiturgicalInformation entities

---
+++ lib/domain/entites/church.dart
@@ -0,0 +1,19 @@
+final class Church {
+  final String name;
+  final String city;
+  final String image;
+
+  const Church({
+    required this.name,
+    required this.city,
+    required this.image,
+  });
+}
+
+final class DetailedChurh extends Church {
+  const DetailedChurh({
+    required super.name,
+    required super.city,
+    required super.image,
+  });
+}
---
+++ lib/domain/entites/liturgical_information.dart
@@ -0,0 +1,11 @@
+final class LiturgicalInformation {
+  final String date;
+  final String week;
+  final String color;
+
+  const LiturgicalInformation({
+    required this.date,
+    required this.week,
+    required this.color,
+  });
+}
