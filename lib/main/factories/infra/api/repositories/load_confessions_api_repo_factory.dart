dec1b1b9a5d594699a555041ed8b4b0dc12bbd35
Henrique Martins <hmartins.dev@gmail.com>
2025-06-02 00:16:54 -0300

    feat: add repository factory methods for Church, Confession, Liturgical Information, and Masses

---
+++ lib/main/factories/infra/api/repositories/load_confessions_api_repo_factory.dart
@@ -0,0 +1,5 @@
+import 'package:diocese_santos/infra/api/repositories/confession_api_repository.dart';
+
+ConfessionApiRepository makeConfessionApiRepository() {
+  return ConfessionApiRepository();
+}
---
+++ lib/main/factories/infra/api/repositories/load_liturgical_informations_api_repo_factory.dart
@@ -0,0 +1,5 @@
+import 'package:diocese_santos/infra/api/repositories/liturgical_information_api_repository.dart';
+
+LiturgicalInformationApiRepository makeLiturgicalInformationApiRepository() {
+  return LiturgicalInformationApiRepository();
+}
