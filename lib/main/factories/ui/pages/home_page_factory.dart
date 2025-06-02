cdb4b3d133ccfc7dcbdf745ce5516682b3df99c8
Henrique Martins <hmartins.dev@gmail.com>
2025-06-02 00:17:08 -0300

    feat(home): enhance HomePage presenter and loading methods for data retrieval

--- lib/main/factories/ui/pages/home_page_factory.dart
+++ lib/main/factories/ui/pages/home_page_factory.dart
@@ -1,9 +1,23 @@
+import 'package:diocese_santos/main/factories/infra/api/repositories/load_churchs_api_repo_factory.dart';
+import 'package:diocese_santos/main/factories/infra/api/repositories/load_confessions_api_repo_factory.dart';
+import 'package:diocese_santos/main/factories/infra/api/repositories/load_liturgical_informations_api_repo_factory.dart';
+import 'package:diocese_santos/main/factories/infra/api/repositories/load_masses_api_repo_factory.dart';
 import 'package:diocese_santos/presentation/presenters/rx/home_rx_presenter.dart';
 import 'package:diocese_santos/ui/pages/home/home_page.dart';
+
 import 'package:flutter/material.dart';
 
-final presenter = HomeRxPresenter();
+final massesRepo = makeMassesApiRepository();
+final churchsRepo = makeChurchsApiRepository();
+final confessionRepo = makeConfessionApiRepository();
+final liturgicalInformationsRepo = makeLiturgicalInformationApiRepository();
 
-Widget makeHomePage() {
-  return HomePage(presenter: presenter);
-}
+final presenter = HomeRxPresenter(
+  liturgicalInformationsLoader:
+      liturgicalInformationsRepo.loadLiturgicalInformation,
+  churchsLoader: churchsRepo.loadChurchs,
+  massesLoader: massesRepo.loadMasses,
+  confessionLoader: confessionRepo.loadConfession,
+);
+
+Widget makeHomePage() => HomePage(presenter: presenter);
--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -7,11 +7,10 @@ import 'package:diocese_santos/ui/pages/home/widgets/list_of_masses.dart';
 import 'package:diocese_santos/ui/pages/home/widgets/liturgical_informations.dart';
 import 'package:diocese_santos/ui/widgets/custom_divider.dart';
 import 'package:diocese_santos/ui/widgets/header.dart';
-import 'package:diocese_santos/ui/widgets/information_card.dart';
 import 'package:diocese_santos/ui/widgets/page_container.dart';
 import 'package:diocese_santos/ui/widgets/quick_access_header.dart';
-import 'package:diocese_santos/ui/widgets/subtitle_section.dart';
 import 'package:diocese_santos/ui/widgets/user_info_header.dart';
+
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
@@ -31,84 +30,106 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    widget.presenter.getLiturgicalInformations();
+    widget.presenter.loadMasses();
+    widget.presenter.loadChurchs();
+    widget.presenter.loadConfession();
+    widget.presenter.loadLiturgicalInformations();
   }
 
   @override
   Widget build(BuildContext context) {
     final statusBarHeight = MediaQuery.of(context).viewPadding.top;
 
-    return PageContainer(
-      showBottomNavigationBar: true,
-      child: Stack(
-        children: [
-          Column(
-            children: [
-              Header(
-                padding: EdgeInsets.only(
-                  left: 24,
-                  top: 10 + statusBarHeight,
-                  right: 24,
-                  bottom: 40,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+    return StreamBuilder<HomeViewModel>(
+        stream: widget.presenter.homeStream,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState != ConnectionState.active) {
+            return Center(child: const CircularProgressIndicator());
+          }
+          if (!snapshot.hasData) {
+            return Center(child: const CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            print('Error: ${snapshot.error}');
+            return Center(child: Text('Erro: ${snapshot.error}'));
+          }
+
+          final viewModel = snapshot.data!;
+          return PageContainer(
+            showBottomNavigationBar: true,
+            child: Stack(
+              children: [
+                Column(
                   children: [
-                    UserInfoHeader(),
-                    Gap(32),
-                    Text(
-                      'Explore a Diocese de Santos',
-                      style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w600,
+                    Header(
+                      padding: EdgeInsets.only(
+                        left: 24,
+                        top: 10 + statusBarHeight,
+                        right: 24,
+                        bottom: 40,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          UserInfoHeader(),
+                          Gap(32),
+                          Text(
+                            'Explore a Diocese de Santos',
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleMedium!
+                                .copyWith(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w600,
+                                ),
                           ),
-                    ),
-                    Gap(16),
-                    SizedBox(
-                      height: 54,
-                      child: TextField(
-                        decoration: InputDecoration(
-                          filled: true,
-                          hintText: 'O que você deseja encontrar?',
-                          hintStyle: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 14,
+                          Gap(16),
+                          SizedBox(
+                            height: 54,
+                            child: TextField(
+                              decoration: InputDecoration(
+                                filled: true,
+                                hintText: 'O que você deseja encontrar?',
+                                hintStyle: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 14,
+                                ),
+                                prefixIcon: const Icon(Icons.search),
+                                prefixIconColor: Colors.blueAccent,
+                                fillColor: Colors.white,
+                              ),
+                            ),
                           ),
-                          prefixIcon: const Icon(Icons.search),
-                          prefixIconColor: Colors.blueAccent,
-                          fillColor: Colors.white,
-                        ),
+                          Gap(32),
+                        ],
                       ),
                     ),
-                    Gap(32),
+                    Gap(54),
+                    LiturgicalInformations(
+                      date: viewModel.liturgicalInformations.date,
+                      week: viewModel.liturgicalInformations.week,
+                      color: viewModel.liturgicalInformations.color,
+                    ),
+                    Gap(8),
+                    DailyLiturgy(),
+                    Gap(16),
+                    FavoriteChurchs(),
+                    CustomDivider(),
+                    ListOfMasses(),
+                    ListOfConfession(),
+                    ConfessionAssistants(),
+                    CustomDivider(),
                   ],
                 ),
-              ),
-              Gap(54),
-              LiturgicalInformations(
-                date: widget.presenter.liturgicalDate,
-                week: widget.presenter.liturgicalWeek,
-                color: widget.presenter.liturgicalColor,
-              ),
-              Gap(8),
-              DailyLiturgy(),
-              Gap(16),
-              FavoriteChurchs(),
-              CustomDivider(),
-              ListOfMasses(),
-              ListOfConfession(),
-              ConfessionAssistants(),
-              CustomDivider(),
-            ],
-          ),
-          Positioned(
-            top: 270,
-            left: 8,
-            right: 8,
-            child: QuickAccessHeader(),
-          ),
-        ],
-      ),
-    );
+                Positioned(
+                  top: 270,
+                  left: 8,
+                  right: 8,
+                  child: QuickAccessHeader(),
+                ),
+              ],
+            ),
+          );
+        });
   }
 }
