5935e331cea34ebf9f923c99828e1899dda101f3
hmartiins <hmartins224@gmail.com>
2025-06-02 18:31:55 -0300

    test: add widget test for SubtitleSection

---
+++ test/ui/widgets/subtitle_section_test.dart
@@ -0,0 +1,21 @@
+import 'package:diocese_santos/ui/widgets/custom_divider.dart';
+import 'package:diocese_santos/ui/widgets/subtitle_section.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  late Widget sut;
+  late String title = "Subtitle Section Test";
+
+  setUp(() {
+    sut = MaterialApp(
+      home: SubtitleSection(title: title),
+    );
+  });
+
+  testWidgets('should render SubtitleSection correctly', (tester) async {
+    await tester.pumpWidget(sut);
+    expect(find.byType(SubtitleSection), findsOneWidget);
+    expect(find.text(title), findsOneWidget);
+  });
+}
